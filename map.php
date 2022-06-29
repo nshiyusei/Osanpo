66361f35015ce847f41ba27e3136c5a0ced1fab8
nshiyusei <nishiyu.kurasei.2109@ezweb.ne.jp>
2022-06-29 14:02:02 +0900

    1

---
+++ map.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>map</title>
+        <!-- BootstrapのCSS読み込み -->
+        <link href="/survey/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+        <div>map</div>
+    </head>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRDP_sdN5n8LI44-vl4CAEm4gnlGF4XH4&callback=initMap"></script>
+
+</html>
