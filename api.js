f3ddf5e776bef125fc3fe5319c6ff6d6f11ba676
shimamamamamamae <shima.k.1203@gmail.com>
2022-07-08 17:31:21 +0900

    愛ふぇうじxjgバイオ下ワインhｇｗ

--- api.js
+++ api.js
@@ -1,117 +1,87 @@
 //apiキー : AIzaSyDRDP_sdN5n8LI44-vl4CAEm4gnlGF4XH4
 
+var lat;
+var lng;
+
 function initMap() {
 	function success(pos) {
-		var lat = pos.coords.latitude;
-		var lng = pos.coords.longitude;
+		lat = pos.coords.latitude;
+		lng = pos.coords.longitude;
 		var latlng = new google.maps.LatLng(lat, lng); //中心の緯度, 経度
-		var map = new google.maps.Map(document.getElementById('map'), {
+		var map = new google.maps.Map(document.getElementById('map1'), {
 			zoom: 17,
 			center: latlng
 		});
 		var marker = new google.maps.Marker({
 			position: latlng, //マーカーの位置（必須）
 			map: map //マーカーを表示する地図
+
+			
 		});
+		initialize();
 	}
 	function fail(error) {
 		alert('位置情報の取得に失敗しました。エラーコード：' + error.code);
-		var latlng = new google.maps.LatLng(35.6812405, 139.7649361); //東京駅
-		var map = new google.maps.Map(document.getElementById('maps'), {
+		lat = 35.6812405;
+		lng = 139.7649361;
+		var latlng = new google.maps.LatLng(lat, lng); //東京駅
+		var map = new google.maps.Map(document.getElementById('map1'), {
 			zoom: 10,
 			center: latlng
 		});
+		initialize();
 	}
 	navigator.geolocation.getCurrentPosition(success, fail);
 }
+
 var mayMap;
 var service;
  
 // マップの初期設定
+var map;
+var service;
+var infowindow;
+
 function initialize() {
-    // Mapクラスのインスタンスを作成（緯度経度は池袋駅に設定）
-    var initPos = new google.maps.LatLng(35.729756, 139.711069);
-    // 地図のプロパティを設定（倍率、マーカー表示位置、地図の種類）
-    var myOptions = {
-        zoom: 15,
-        center: initPos,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    // #map_canva要素にMapクラスの新しいインスタンスを作成
-    myMap = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    // 検索の条件を指定（緯度経度、半径、検索の分類）
-    var request = {
-        location: initPos,
-        radius: 1000,      // ※１ 表示する半径領域を設定(1 = 1M)
-        types: ['cafe']    // ※２ typesプロパティの施設タイプを設定
-    };
-    var service = new google.maps.places.PlacesService(myMap);
-    service.search(request, Result_Places);
+  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+alert(lat);
+  map = new google.maps.Map(document.getElementById('map2'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    type: ['park']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
- 
-// 検索結果を受け取る
-function Result_Places(results, status){
-    // Placesが検家に成功したかとマうかをチェック
-    if(status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            // 検索結果の数だけ反復処理を変数placeに格納
-            var place = results[i];
-            createMarker({
-                 text : place.name,
-                 position : place.geometry.location
-            });
-        }
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+		console.log(results[i].geometry.location);
+      	createMarker(results[i]);
     }
+  }
 }
  
-// 入力キーワードと表示範囲を設定
-function SearchGo() {
-    var initPos = new google.maps.LatLng(0,0);
-    var mapOptions = {
-        center : initPos,
-        zoom: 0,
-        mapTypeId : google.maps.MapTypeId.ROADMAP
-    };
-    // #map_canva要素にMapクラスの新しいインスタンスを作成
-    myMap = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    service = new google.maps.places.PlacesService(myMap);
-    // input要素に入力されたキーワードを検索の条件に設定
-    var myword = document.getElementById("search");
-    var request = {
-        query : myword.value,
-        radius : 5000,
-        location : myMap.getCenter()
-    };
-    service.textSearch(request, result_search);
-}
- 
-// 検索の結果を受け取る
-function result_search(results, status) {
-    var bounds = new google.maps.LatLngBounds();
-    for(var i = 0; i < results.length; i++){
-        createMarker({
-             position : results[i].geometry.location,
-             text : results[i].name,
-             map : myMap
-         });
-        bounds.extend(results[i].geometry.location);
-    }
-    myMap.fitBounds(bounds);
-}
- 
+
 // 該当する位置にマーカーを表示
 function createMarker(options) {
-    // マップ情報を保持しているmyMapオブジェクトを指定
-    options.map = myMap;
-    // Markcrクラスのオブジェクトmarkerを作成
-    var marker = new google.maps.Marker(options);
-    // 各施設の吹き出し(情報ウインドウ)に表示させる処理
-    var infoWnd = new google.maps.InfoWindow();
-    infoWnd.setContent(options.text);
-    // addListenerメソッドを使ってイベントリスナーを登録
-    google.maps.event.addListener(marker, 'click', function(){
-        infoWnd.open(myMap, marker);
-    });
-    return marker;
+	// 緯度経度を取得
+	var latlng = options.geometry.location;
+	// 住所を取得
+	var address = options.formatted_address;
+
+	var marker = new google.maps.Marker({
+		position: latlng, //マーカーの位置（必須）
+		map: map //マーカーを表示する地図
+	});
 }
  
+// ページ読み込み完了後、Googleマップを表示
+initMap();
--- map.php
+++ map.php
@@ -1,20 +1,48 @@
-
 <!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <title>Google Maps API サンプル</title>
-  </head>
-  <body>
-    <input type="text" size="55" id="search" value="近くの公園" />
-    <input type="button" size="55" value="検索" onClick="SearchGo()" />
-    <div id="map_canvas" style="width: 100%; height: 90%;"></div>
+<html>
+    <head>
+<style>/*
+        <!--ここは触らない-->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>map</title>
+        <!-- BootstrapのCSS読み込み -->
+        <link href="/survey/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+        <div>map</div>
+        <!--ここは触らない-->
+        
+        <style>
+            h2 {color: green;}
+        </style>
+*/</style>
+    </head>
+
+    <body>
+        <h2 style = "text-align:center">
+            お散歩時間を入力してください
+        </h2>
+
+        <form method = "POST" action = "map_test.php" style = "text-align: center">
+            <input type = "number" placeholder = "時間を入力してください" name = "osampo_time"  min = "5" value = "25">
+            <input type = "submit" name = "btn_confirm" value = "決定">
+        </form>
+
+        <?php if(empty($_POST['osampo_time'])) : ?>
+            <p style = "text-align: center; color: red">時間が入力されていません！</p>
+        <?php endif; ?>
     
-    <div id="map" style="width:600px; height:400px"></div>
+        <div id="map1" style="width:600px; height:400px"></div>
+        <div id="map2" style="width:600px; height:400px"></div>
+
+
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRDP_sdN5n8LI44-vl4CAEm4gnlGF4XH4&libraries=places"></script>
+        <script defer type="text/javascript" src="api.js?t=<?php echo time(); ?>"></script>
+        
+
     
-    <script defer type="text/javascript" src="api.js?t=<?php echo time(); ?>">
-    </script>
-    <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRDP_sdN5n8LI44-vl4CAEm4gnlGF4XH4&callback=initMap"></script> 
-  </body>
+    </body>
+
 </html>
