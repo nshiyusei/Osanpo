a564a532b5df9f0e404f53cd4adcaf8e01ec404c
shimamamamamamae <shima.k.1203@gmail.com>
2022-07-13 14:07:50 +0900

    center

--- map.php
+++ map.php
@@ -1,32 +1,48 @@
 <!DOCTYPE html>
 <html>
     <head>
-<style>/*
+
         <!--ここは触らない-->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>map</title>
-        <!-- BootstrapのCSS読み込み -->
-        <link href="/survey/css/bootstrap.min.css" rel="stylesheet">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
-        <div>map</div>
-        <!--ここは触らない-->
         
+        <!--追記:body,body:after-->
         <style>
             h2 {color: green;}
+
+            body{
+                height: 100%;
+            }
+
+            body:after{
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                content: "";
+                background: #AAFF00;
+                background: -moz-linear-gradient(top, #AAFF00 0%, #fff 100%);
+                background: -webkit-linear-gradient(top, #AAFF00 0%, #fff 100%);
+                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr = '#AAFF00', endColorstr = '#000', GradientType = 0);
+                background: linear-gradient(to bottom, #AAFF00 0%, #fff 100%);
+                z-index: -1;
+            }
         </style>
-*/</style>
+
     </head>
 
     <body>
-        <h2 style = "text-align:center">
+        <h2 style = "text-align:center; font-weight:bold">
             お散歩時間を入力してください
         </h2>
 
+
         <form method = "POST" action=? style = "text-align: center">
             <input type = "number" placeholder = "時間を入力してください" name = "osampo_time"  min = "5" value = "60">
+
             <input type = "submit" name = "btn_confirm" value = "決定">
         </form>
 
@@ -34,15 +50,19 @@
             <p style = "text-align: center; color: red">時間が入力されていません！</p>
         <?php endif; ?>
 
+
+        
+
         <script type="text/javascript"> var walktime = '<?php echo $_POST['osampo_time']; ?>';</script>
     
-        <div id="map1" style="width:600px; height:400px"></div>
-        <div id="map2" style="width:600px; height:400px"></div>
-        
+        <div id="map1" style="width:600px; height:400px;margin:auto;"></div>
+        <div id="map2" style="width:600px; height:400px;margin:auto;"></div>
+
         <script initMap></script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRDP_sdN5n8LI44-vl4CAEm4gnlGF4XH4&libraries=places"></script>
         <script defer type="text/javascript" src="api.js?t=<?php echo time(); ?>"></script>
         
+
 
     
     </body>
